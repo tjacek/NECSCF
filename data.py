e3021881182baed3c2038fcb2cb60f92500c841f
tjacek <tjacek@agh.edu.pl>
2023-09-20 23:33:30 +0200

    read arff

--- data.py
+++ data.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
+from scipy.io import arff
+from sklearn import preprocessing
 
 def get_dataset(data_path):
     df=pd.read_csv(data_path,header=None) 
     return prepare_data(df)
 
-def prepare_data(df):
-    X=df.iloc[:,:-1]
+def prepare_data(df,target=-1):
+    X=df.iloc[:,:target]
+    print(X)
     X=X.to_numpy()
     X=preprocessing.scale(X) # data preprocessing
     y=df.iloc[:,-1]
@@ -20,3 +23,16 @@ def get_dataset_params(X,y):
             'dims':X.shape[1],
             'batch':X.shape[0],
             'class_weights':dict(Counter(y))}
+
+def from_arff(in_path:str):
+    raw_rows,raw_cats = arff.loadarff(in_path)   
+    rows=[ [float(r_j) for r_j in raw_i]
+           for raw_i in raw_rows]    
+#    X=np.array(dataset[0])
+    cols=[col_j.split('\'')[0] for col_j in raw_cats] 
+    df=pd.DataFrame(rows,columns=cols)
+    print(df.head())
+    
+if __name__ == '__main__':
+    in_path='raw/compas.arff'
+    from_arff(in_path)
