171a8eb468b5098b07482b8d88846aa33bce7760
tjacek <tjacek@agh.edu.pl>
2023-09-24 20:19:54 +0200

    fix preproc

--- data.py
+++ data.py
@@ -2,20 +2,22 @@ import numpy as np
 import pandas as pd
 from scipy.io import arff
 from sklearn import preprocessing
-
+from collections import Counter
 def get_dataset(data_path):
     df=pd.read_csv(data_path,header=None) 
     return prepare_data(df)
 
 def prepare_data(df,target=-1):
-    X=df.iloc[:,:target]
-    print(X)
-    X=X.to_numpy()
+#    X=df.iloc[:,::target]
+    X=df.to_numpy()
+    X=np.delete(X,[target], axis=1)
+    X=np.nan_to_num(X)
     X=preprocessing.scale(X) # data preprocessing
-    y=df.iloc[:,-1]
+    y=df.iloc[:,target]
     cats={ cat_i:i for i,cat_i in enumerate(y.unique())}
     y=y.to_numpy()
     y=[cats[y_i] for y_i in y]
+#    raise Exception(get_dataset_params(X,np.array(y)))
     return X,np.array(y)
 
 def get_dataset_params(X,y):
@@ -28,10 +30,9 @@ def from_arff(in_path:str):
     raw_rows,raw_cats = arff.loadarff(in_path)   
     rows=[ [float(r_j) for r_j in raw_i]
            for raw_i in raw_rows]    
-#    X=np.array(dataset[0])
     cols=[col_j.split('\'')[0] for col_j in raw_cats] 
     df=pd.DataFrame(rows,columns=cols)
-    print(df.head())
+    return df
     
 if __name__ == '__main__':
     in_path='raw/compas.arff'
