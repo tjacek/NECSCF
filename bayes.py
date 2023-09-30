4d11c4a970aff1e682ce25bd3bc9a7618d33ae69
tjacek <tjacek@agh.edu.pl>
2023-09-30 11:58:26 +0200

    optim_rf

--- bayes.py
+++ bayes.py
@@ -1,12 +1,30 @@
 from skopt.space import Real, Integer
 from skopt.utils import use_named_args
 from skopt import gp_minimize
+from sklearn import ensemble
+from sklearn.metrics import accuracy_score
 import data,train
 
 def optim_rf(spilt):
-    'max_depth': [10, 20] 
+    space=[Integer(5,20,name='max_depth'),
+           Integer(5,20,name='n_estimators')] 
+    x_train,y_train=spilt.get_train()
+    x_valid,y_valid=spilt.get_valid()
 
-
+    @use_named_args(space)
+    def objective(**params):
+#         clf.set_params(**params)
+        print(params)
+        clf=ensemble.RandomForestClassifier(**params)#class_weight='balanced_subsample')
+        clf.fit(x_train,y_train)
+        y_pred=clf.predict(x_valid)
+        return accuracy_score(y_valid,y_pred)
+    res_gp = gp_minimize(objective, 
+                         space, 
+                         n_calls=50, 
+                         random_state=0)
+    print("Best score=%.4f" % res_gp.fun)
+         
 if __name__ == '__main__':
     name='arrhythmia'
     in_path=f'raw/{name}.arff'
@@ -17,4 +35,4 @@ if __name__ == '__main__':
     	                  y=y,
     	                  n_iters=1)
     spilt=list(spilt)[0]
-    print(type(spilt))
+    optim_rf(spilt)
