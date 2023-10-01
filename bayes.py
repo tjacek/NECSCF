8e2db929c1e83852fa7440558e3bfb37b0f59127
tjacek <tjacek@agh.edu.pl>
2023-10-01 02:04:42 +0200

    bayes eval

--- bayes.py
+++ bayes.py
@@ -3,9 +3,42 @@ from skopt.utils import use_named_args
 from skopt import gp_minimize
 from sklearn import ensemble
 from sklearn.metrics import accuracy_score
-import data,train
+import data,pred
 
-def optim_rf(spilt):
+
+def eval_bayes(exp,verbose=0):
+    extractor= exp.make_extractor()
+    data_tuples=exp.split.extract(extractor=extractor,
+                                       use_valid=True)
+    (x_train,y_train),(x_valid,y_valid),(x_test,y_test)=data_tuples
+    space=[Integer(5,20,name='max_depth'),
+           Integer(5,20,name='n_estimators')]
+    @use_named_args(space)
+    def objective(**params):
+#        print("call")
+        votes=[]
+        for train_i,valid_i in zip(x_train,x_valid):
+            clf_i=ensemble.RandomForestClassifier(**params)
+            clf_i.fit(train_i,y_train)
+            votes.append(clf_i.predict_proba(valid_i))
+        y_pred=pred.count_votes(votes)
+        return -accuracy_score(y_valid,y_pred)
+
+    res_gp = gp_minimize(objective, 
+                         space, 
+                         n_calls=50, 
+                         random_state=0)            
+    params={space_i.name:x_i 
+            for space_i,x_i in zip(space,res_gp.x)}
+    votes=[]
+    for train_i,test_i in zip(x_train,x_test):
+            clf_i=ensemble.RandomForestClassifier(**params)
+            clf_i.fit(train_i,y_train)
+            votes.append(clf_i.predict_proba(test_i))
+    y_pred=pred.count_votes(votes)
+    return y_test,y_pred
+
+def optim_rf(spilt,verbose=0):
     space=[Integer(5,20,name='max_depth'),
            Integer(5,20,name='n_estimators')] 
     x_train,y_train=spilt.get_train()
@@ -13,18 +46,31 @@ def optim_rf(spilt):
 
     @use_named_args(space)
     def objective(**params):
-#         clf.set_params(**params)
-        print(params)
+        if(verbose):
+            print(params)
         clf=ensemble.RandomForestClassifier(**params)#class_weight='balanced_subsample')
         clf.fit(x_train,y_train)
         y_pred=clf.predict(x_valid)
-        return accuracy_score(y_valid,y_pred)
+        return -accuracy_score(y_valid,y_pred)
     res_gp = gp_minimize(objective, 
                          space, 
                          n_calls=50, 
                          random_state=0)
+    print(dir(res_gp))
+    print(res_gp.x)
     print("Best score=%.4f" % res_gp.fun)
-         
+    return res_gp.x,res.fun
+
+def eval_optim(spilt):
+    x_train,y_train=spilt.get_train()
+    x_valid,y_valid=spilt.get_valid()
+
+    clf=ensemble.RandomForestClassifier()
+    clf.fit(x_train,y_train)
+    y_pred=clf.predict(x_valid)
+    print("Base score=%.4f" % accuracy_score(y_valid,y_pred))
+    optim_rf(spilt)
+
 if __name__ == '__main__':
     name='arrhythmia'
     in_path=f'raw/{name}.arff'
@@ -35,4 +81,4 @@ if __name__ == '__main__':
     	                  y=y,
     	                  n_iters=1)
     spilt=list(spilt)[0]
-    optim_rf(spilt)
+    eval_optim(spilt)
--- eval.py
+++ eval.py
@@ -1,14 +1,21 @@
 import numpy as np
 import utils
 
-def all_exp(in_path):
+def all_exp(in_path,stats=True):
     for path_i in utils.top_files(in_path):
         name_i=path_i.split('/')[-1]
-        mean_i,std_i=get_stats(path_i,
-        	                   verbose=0)
-        print(f'{name_i},{mean_i:.2f},{std_i:.2f}')
-        	
-def get_stats(in_path,verbose=1):
+        result_i=get_stats(path_i,
+        	               verbose=0,
+                           stats=stats)
+        if(stats):
+            mean_i,std_i=result_i
+            print(f'{name_i},{mean_i:.4f},{std_i:.4f}')
+        else:
+            result_i.sort()
+            print(name_i+','.join([f'{acc_j:.4}' 
+                        for acc_j in result_i][-2:]))
+
+def get_stats(in_path,verbose=1,stats=False):
     metric=utils.get_metric('acc')
     acc=[]
     for path_i in utils.top_files(in_path):
@@ -17,12 +24,15 @@ def get_stats(in_path,verbose=1):
 #        print(result_i['pred'])
         acc_i=metric(result_i['true'],result_i['pred'])
         acc.append(acc_i)
+    if(not stats):
+        return acc
     mean_i,std_i=np.mean(acc),np.std(acc)
     if(verbose):
         print(f'Mean:{mean_i}')
         print(f'Std:{std_i}')
-    return mean_i,std_i
+    return mean_i,np.max(acc)#std_i
 
 #name='cnae-9'
-#get_stats(f'../OML/pred/{name}')
-all_exp('../OML/pred')
+
+all_exp('../OML/pred',False)
+all_exp('../OML/optim_pred',False)
