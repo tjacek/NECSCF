3cd3d75f8078f464e8edf4516082e26cb920d268
tjacek <tjacek@agh.edu.pl>
2023-10-04 23:03:30 +0200

    optimised param exp

--- eval.py
+++ eval.py
@@ -1,27 +1,33 @@
 import numpy as np
+import argparse
 import utils
 
-def all_exp(in_path,stats=True):
-    for path_i in utils.top_files(in_path):
-        name_i=path_i.split('/')[-1]
-        result_i=get_stats(path_i,
-        	               verbose=0,
-                           stats=stats)
-        if(stats):
-            mean_i,std_i=result_i
-            print(f'{name_i},{mean_i:.4f},{std_i:.4f}')
-        else:
-            result_i.sort()
-            print(name_i+','.join([f'{acc_j:.4}' 
-                        for acc_j in result_i][-2:]))
+def all_exp(in_path,stats=False):
+#    for path_i in utils.top_files(in_path):
+#        name_i=path_i.split('/')[-1]
+    metric=utils.get_metric('acc')
+    @utils.dir_fun
+    def helper(in_path,out_path):
+        result_i=np.load(in_path)
+        acc_i=metric(result_i['true'],result_i['pred'])
+        print(f'{in_path}:{acc_i:.4f}')
+#        result_i=get_stats(in_path,#path_i,
+#        	               verbose=0,
+#                           stats=stats)
+#        if(stats):
+#            mean_i,std_i=result_i
+#            print(f'{name_i},{mean_i:.4f},{std_i:.4f}')
+#        else:
+#            result_i.sort()
+#            print(name_i+','.join([f'{acc_j:.4}' 
+#                        for acc_j in result_i][-2:]))
+    helper(in_path,'out')   
 
 def get_stats(in_path,verbose=1,stats=False):
     metric=utils.get_metric('acc')
     acc=[]
     for path_i in utils.top_files(in_path):
         result_i=np.load(path_i)
-#        print(result_i['true'])
-#        print(result_i['pred'])
         acc_i=metric(result_i['true'],result_i['pred'])
         acc.append(acc_i)
     if(not stats):
@@ -33,6 +39,6 @@ def get_stats(in_path,verbose=1,stats=False):
     return mean_i,np.max(acc)#std_i
 
 #name='cnae-9'
-
-all_exp('../OML/pred',False)
-all_exp('../OML/optim_pred',False)
+if __name__ == '__main__':
+#    all_exp('../OML_reduce/pred',False)
+    all_exp('../OML_reduce/pred_optim',False)
