ee5d781624dea9b7474bee116e2fd3e5c829e8bb
tjacek <jacekt89@gmail.com>
2024-11-25 22:37:19 +0100

    Class Ens factory

--- exp.py
+++ exp.py
@@ -2,23 +2,38 @@ import numpy as np
 import tensorflow as tf
 import base,dataset,deep,utils
 
-class NECSCF(object):
-    def __init__(self, model=None):
+class ClassEnsFactory(object):
+    def __init__(self,hyper_params=None):
+        if(hyper_params is None):
+           hyper_params={'layers':1,'units_0':2,'batch':True}
+        self.params=None
+        self.hyper_params=hyper_params
+    
+    def init(self,data):
+        self.params={'dims': (data.dim(),),
+                     'n_cats':data.n_cats(),
+                     'class_weights':dataset.get_class_weights(data.y) }
+
+    def __call__(self):
+        return ClassEns(params=self.params,
+                        hyper_params=self.hyper_params)
+
+class ClassEns(object):
+    def __init__(self, params,
+                       hyper_params,
+                       model=None):
+		self.params=params
+        self.hyper_params=hyper_params
         self.model = model
-		
+
     def fit(self,X,y):
         data=dataset.Dataset(X,y)
         if(self.model is None):
-            params={'dims': (data.dim(),),
-                    'n_cats':data.n_cats(),
-                    'class_weights':dataset.get_class_weights(data.y) }
-            self.model=deep.ensemble_builder(params,
-	                                         hyper_params=None,
+            self.model=deep.ensemble_builder(params=self.params,
+	                                         hyper_params=self.hyper_params,
 	                                         alpha=0.5)
-        y=[tf.one_hot(y,
-                      depth=params['n_cats'])
+        y=[tf.one_hot(y,depth=params['n_cats'])
                 for i in range(data.n_cats())]
-
         self.model.fit(x=X,
         	           y=y,
         	           callbacks=deep.get_callback())
@@ -30,7 +45,7 @@ class NECSCF(object):
 
 @utils.elapsed_time
 def ensemble_exp(in_path,
-                 n_splits=3,
+                 n_splits=10,
                  n_repeats=1):
     data=dataset.read_csv(in_path)
     protocol=base.get_protocol("unaggr")(n_splits,n_repeats)
@@ -43,7 +58,7 @@ def ensemble_exp(in_path,
     balance=np.mean([result_j.get_balanced() for result_j in results])
     print(f"{acc},{balance}")
 
-ensemble_exp(in_path="../uci/cmc")
+ensemble_exp(in_path="../uci/wine-quality-red")
 #clf.fit(data.X,data.y)
 #clf.predict(data.X)
 #model.summary()
