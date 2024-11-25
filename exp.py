8c6fc90e0c715a898f9663dcfbc5ea75766f118a
tjacek <jacekt89@gmail.com>
2024-11-25 23:47:29 +0100

    fix ensemble_exp

--- exp.py
+++ exp.py
@@ -22,7 +22,7 @@ class ClassEns(object):
     def __init__(self, params,
                        hyper_params,
                        model=None):
-		self.params=params
+        self.params=params
         self.hyper_params=hyper_params
         self.model = model
 
@@ -32,7 +32,7 @@ class ClassEns(object):
             self.model=deep.ensemble_builder(params=self.params,
 	                                         hyper_params=self.hyper_params,
 	                                         alpha=0.5)
-        y=[tf.one_hot(y,depth=params['n_cats'])
+        y=[tf.one_hot(y,depth=self.params['n_cats'])
                 for i in range(data.n_cats())]
         self.model.fit(x=X,
         	           y=y,
@@ -50,9 +50,11 @@ def ensemble_exp(in_path,
     data=dataset.read_csv(in_path)
     protocol=base.get_protocol("unaggr")(n_splits,n_repeats)
     splits=protocol.get_split(data)
+    clf_factory=ClassEnsFactory()
+    clf_factory.init(data)
     results=[]
     for split_k in splits:
-        clf_k=NECSCF()
+        clf_k=clf_factory()
         results.append(split_k.eval(data,clf_k))
     acc=np.mean([result_j.get_acc() for result_j in results])
     balance=np.mean([result_j.get_balanced() for result_j in results])
