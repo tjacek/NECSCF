8d54e22bcfe83d451faf3dfa43c1649002692d1c
tjacek <jacekt89@gmail.com>
2024-12-11 14:36:56 +0100

    save multiple plot in eval

--- eval.py
+++ eval.py
@@ -32,28 +32,44 @@ def all_exp(data_path,
         return np.mean(acc,axis=1)
     acc=helper(data_path,exp_path)   
     print(acc)
-#    utils.make_dir(out_path)
-    all_subplots=[]
-    for k in range(n_iters):
+    utils.make_dir(out_path)
+    all_subplots=[[] for k in range(n_iters)]
+    for i,name_i in enumerate(acc.keys()):
+        all_subplots[(i%n_iters)].append(name_i)
+    for i,subplot_i in enumerate(all_subplots):
         _, ax_k = plt.subplots()
-        all_subplots.append(ax_k)
-    for i,(name_i,acc_i) in enumerate(acc.items()):
-        x_order=np.arange(acc_i.shape[0])+1
-        k=(i % n_iters)
-        all_subplots[k].plot(x_order,acc_i,
-                             label=name_i.split("/")[-1])
-    for k,ax_k in enumerate(all_subplots):
+        for name_j in subplot_i:
+            acc_j=acc[name_j]
+            x_order=np.arange(acc_j.shape[0])+1
+            ax_k.plot(x_order,acc_j,
+                      label=name_j.split("/")[-1])
         plt.xlabel("n_clf")
         plt.ylabel("acc")
-        ax_k.legend()
-    plt.savefig(f"{out_path}/{k}")
-    plt.show()
+        plt.legend()
+        plt.savefig(f"{out_path}/{i}")
+        plt.show()
+        plt.clf() 
+    
+#    for k in range(n_iters):
+#        _, ax_k = plt.subplots()
+#        all_subplots.append(ax_k)
+#    for i,(name_i,acc_i) in enumerate(acc.items()):
+#        x_order=np.arange(acc_i.shape[0])+1
+#        k=(i % n_iters)
+#        all_subplots[k].plot(x_order,acc_i,
+#                             label=name_i.split("/")[-1])
+#    for k,ax_k in enumerate(all_subplots):
+#        plt.xlabel("n_clf")
+#        plt.ylabel("acc")
+#        ax_k.legend()
+#    plt.savefig(f"{out_path}/{k}")
+#    plt.show()
 
 def selection(order):
     return [order[:i+1] for i in range(len(order))]
 
 if __name__ == '__main__':
     all_exp("../uci/",#wine-quality-red,
-            "test_exp",#wine-quality-red',
+            "full_models",#wine-quality-red',
             "purity.json",
             "acc_plot")
