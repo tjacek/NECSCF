e53661648cc002bcd1d96f5e99d496d3c2a36828
tjacek <jacekt89@gmail.com>
2024-12-11 01:39:48 +0100

    select subsets

--- eval.py
+++ eval.py
@@ -1,10 +1,11 @@
 import numpy as np
 import argparse
-import dataset,pred,utils,ens
+import dataset,ens,exp,pred,utils
 
 def all_exp(data_path,exp_path,json_path):
     purity_dict=utils.read_json(json_path)
-#    @utils.dir_fun
+    @utils.DirFun({"data_path":0,"model_path":1},
+                  input_arg='data_path')
     def helper(data_path,exp_path):
         name=data_path.split("/")[-1]
         order= purity_dict[name]
@@ -12,17 +13,20 @@ def all_exp(data_path,exp_path,json_path):
         data=dataset.read_csv(data_path)
         ens_factory=ens.ClassEnsFactory()
         ens_factory.init(data)
-        acc=[]
+        acc=[[] for _ in order]
         for split_i,clf_i in pred.model_iter(exp_path,ens_factory):
-            result_i=split_i.pred(data,clf_i)
-            acc.append(result_i.get_acc())
-            print(acc[-1])
-    helper(data_path,exp_path)   
+            for j,subset_j in enumerate(clf_selection):
+                clf_j=exp.SelectedEns(clf_i,subset_j)
+                acc[j].append(split_i.pred(data,clf_j).get_acc())
+        acc=np.array(acc)
+        return np.mean(acc,axis=1)
+    acc=helper(data_path,exp_path)   
+    print(acc)
 
 def selection(order):
     return [order[:i+1] for i in range(len(order))]
 
 if __name__ == '__main__':
-    all_exp("../uci/wine-quality-red",
-            'test_exp/wine-quality-red',
+    all_exp("../uci/",#wine-quality-red,
+            "test_exp",#wine-quality-red',
             "purity.json")
