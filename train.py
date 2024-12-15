f790041fd3e1dfe93a657867d028e72297f400a6
tjacek <jacekt89@gmail.com>
2024-12-15 21:41:00 +0100

    train based on splits

--- train.py
+++ train.py
@@ -2,31 +2,47 @@ import utils
 utils.silence_warnings()
 import numpy as np
 from tqdm import tqdm
-import gc#,ctypes
+#import gc#,ctypes
 import argparse
-import base,ens,utils
+import base,dataset,ens
 
-#@utils.DirFun({"data_path":0,"out_path":1},input_arg='data_path')
-def train_models(data_path,
-                 out_path,
-                 n_splits=10,
-                 n_repeats=10):
-    data_split=base.get_splits(data_path=data_path,
-                           n_splits=n_splits,
-                           n_repeats=n_repeats)
+def base_train(data_path:str,
+               out_path:str,
+               start=0,
+               step=10):
+    data=dataset.read_csv(data_path)
+    split_path=f"{out_path}/splits"
     clf_factory=ens.ClassEnsFactory()
-    clf_factory.init(data_split.data)
-    utils.make_dir(out_path)
-    for i,split_i in tqdm(enumerate(data_split.splits)):
-        out_i=f"{out_path}/{i}"
-        clf_i=clf_factory()
-        clf_i=split_i.fit_clf(data_split.data,clf_i)
-        utils.make_dir(out_i)
-        split_i.save(f"{out_i}/split")
-        clf_i.save(f"{out_i}/model.h5")
-        del clf_i
-        gc.collect()
-#        malloc_trim()
+    clf_factory.init(data)
+    model_path=f"{out_path}/class_ens"
+    utils.make_dir(model_path)
+    for j in tqdm(range(step)):
+        index=start+j
+        raw_split=np.load(f"{split_path}/{index}.npz")
+        split_j=base.UnaggrSplit.Split(train_index=raw_split["arr_0"],
+                                       test_index=raw_split["arr_1"])
+        clf_j=clf_factory()
+        clf_j=split_j.fit_clf(data,clf_j)  
+        clf_j.save(f"{model_path}/{index}.keras")
+#def train_models(data_path,
+#                 out_path,
+#                 n_splits=10,
+#                 n_repeats=10):
+#    data_split=base.get_splits(data_path=data_path,
+#                           n_splits=n_splits,
+#                           n_repeats=n_repeats)
+#    clf_factory=ens.ClassEnsFactory()
+#    clf_factory.init(data_split.data)
+#    utils.make_dir(out_path)
+#    for i,split_i in tqdm(enumerate(data_split.splits)):
+#        out_i=f"{out_path}/{i}"
+#        clf_i=clf_factory()
+#        clf_i=split_i.fit_clf(data_split.data,clf_i)
+#        utils.make_dir(out_i)
+#        split_i.save(f"{out_i}/split")
+#        clf_i.save(f"{out_i}/model.h5")
+#        del clf_i
+#        gc.collect()
 
 #def malloc_trim():
 #    ctypes.CDLL('libc.so.6').malloc_trim(0) 
@@ -35,8 +51,7 @@ def train_models(data_path,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=str, default="../uci/cleveland")
-    parser.add_argument("--model", type=str, default="../full_exp/cleveland")
+    parser.add_argument("--out_path", type=str, default="exp_deep/cleveland")
     args = parser.parse_args()
-    print(args)
-    train_models(data_path=args.data,
-                 out_path=args.model)
+    base_train(data_path=args.data,
+               out_path=args.out_path)
