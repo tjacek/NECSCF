23fc43ff3c1c7557402852edd51bfa2e0aac15df
tjacek <jacekt89@gmail.com>
2024-12-15 20:12:33 +0100

    gen_splits

--- preproc.py
+++ preproc.py
@@ -1,36 +1,29 @@
+import utils
+utils.silence_warnings()
 import numpy as np
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
-import data,utils,train
+import argparse
+import base
 
-def compare(in_path,select):
-    acuracy=utils.get_metric('acc')
-    for path_i in utils.top_files(in_path):
-        X,y=data.get_dataset(path_i)
-        split_i= list(train.gen_split(X,y,n_iters=1))[0]
-        y_true,y_pred= split_i.eval('RF')
-        acc_i=acuracy(y_true,y_pred)
-        print(f'{acc_i:.4f},{X.shape[1]}')
-        X=select(X,y)
-        split_i.X=X
-        y_true,y_pred= split_i.eval('RF')
-        acc_i=acuracy(y_true,y_pred)
-        print(f'{acc_i:.4f},{X.shape[1]}')      
+def gen_splits(data_path,
+               out_path,
+               n_splits=10,
+               n_repeats=10):
+    utils.make_dir(out_path)
+    @utils.DirFun({"in_path":0,"out_path":1})
+    def helper(in_path,out_path):
+        utils.make_dir(out_path)
+        data_split=base.get_splits(data_path=in_path,
+                                   n_splits=n_splits,
+                                   n_repeats=n_repeats,
+                                   split_type="unaggr")
+        split_path=f"{out_path}/splits"
+        utils.make_dir(split_path)
+        for i,split_i in enumerate(data_split.splits):
+            split_i.save(f"{split_path}/{i}")
+    helper(data_path,out_path)
 
-def transform(in_path,out_path,select):
-    utils.make_dir(out_path)	
-    for path_i in utils.top_files(in_path):
-        name_i=path_i.split('/')[-1]
-        X,y=data.get_dataset(path_i)
-        X=select(X,y)
-        np.savez(file=f'{out_path}/{name_i}',
-                 X=X,
-                 y=y)
-
-def kbest(X,y,k=100):
-    if(X.shape[1]<k):
-        return X	
-    return SelectKBest(f_classif, k=k).fit_transform(X, y)
-
-#compare('raw',kbest)
-transform('raw','redu',kbest)
+if __name__ == '__main__':
+    gen_splits(data_path="../uci",
+               out_path="exp_deep",
+               n_splits=10,
+               n_repeats=10)
