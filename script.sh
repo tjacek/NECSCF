83e600e20cab43792602eb4cc27e845cad2a3264
tjacek <jacekt89@gmail.com>
2024-12-16 14:15:09 +0100

    iteration in script

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 data_path='../uci'
-out_path='full_models'
+out_path='exp_deep'
 
 mkdir ${out_path}
-
+#step=20
 
 for data_i in "$data_path"/* 
 do
@@ -10,5 +10,10 @@ do
   name_i="${arr[-1]}"
   out_i="${out_path}/${name_i}"
   echo "$out_i"
-  python3 train.py --data ${data_i} --model ${out_i}
+  for j in {0..4}
+  do
+    let start=j*20
+    python3 train.py --data ${data_i} --out_path ${out_i} --start ${start} --step "20"
+  done
+#  python3 train.py --data ${data_i} --model ${out_i}
 done
