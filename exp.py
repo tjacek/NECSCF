6bcf87e433e6dda213a091519d1c0d97fa1bfd8f
tjacek <jacekt89@gmail.com>
2025-01-20 23:58:52 +0100

    new single_exp

--- exp.py
+++ exp.py
@@ -14,19 +14,31 @@ def single_exp(in_path,
                ens_type="class_ens"):
     utils.make_dir(out_path)
     data_split=get_splits(in_path,out_path)
-    result_path=f"{out_path}/{ens_type}"
-    if(os.path.isdir(result_path)):
-        result_group=dataset.read_result_group(result_path)
-    else:
-        print(f"Train ens{ens_type}")
-        clf_factory=ens.get_ens(ens_type)
-        clf_factory.init(data_split.data)
-        result_group,history=data_split(clf_factory)
-        result_group.save(result_path)
-        history=utils.mean_dict(history)
-        print(history)
-    print(f"Acc:{result_group.get_acc()}")
-    print(f"Balance{result_group.get_acc()}")
+    ens_path=f'{out_path}/{ens_type}'
+    clf_factory=ens.get_ens(ens_type)
+#    clf_factory.init(data_split.data)
+    utils.make_dir(ens_path)
+    for clf_i,history_i in data_split.get_clfs(clf_factory):
+        print(type(clf_i))
+        print(type(history_i))
+#def single_exp(in_path,
+#               out_path,
+#               ens_type="class_ens"):
+#    utils.make_dir(out_path)
+#    data_split=get_splits(in_path,out_path)
+#    result_path=f"{out_path}/{ens_type}"
+#    if(os.path.isdir(result_path)):
+#        result_group=dataset.read_result_group(result_path)
+#    else:
+#        print(f"Train ens{ens_type}")
+#        clf_factory=ens.get_ens(ens_type)
+#        clf_factory.init(data_split.data)
+#        result_group,history=data_split(clf_factory)
+#        result_group.save(result_path)
+#        history=utils.mean_dict(history)
+#        print(history)
+#    print(f"Acc:{result_group.get_acc()}")
+#    print(f"Balance{result_group.get_acc()}")
 
 def eval_exp(exp_path="single_exp"):
     for path_i in utils.top_files(exp_path):
@@ -43,7 +55,7 @@ def get_splits(in_path,out_path):
         return base.read_data_split(in_path,split_path)
     else:
         print("Make splits")
-        data_split=base.make_splits(data_path=in_path,
+        data_split=base.get_splits(data_path=in_path,
                                     n_splits=10,
                                     n_repeats=1,
                                     split_type="unaggr")
@@ -86,32 +98,6 @@ def history_exp(in_path):
     for key_i,acc_i in hist_dict.items():
         print(f"{key_i}-{acc_i:.4f}")
 
-#def selection_exp(in_path,
-#                  n_splits=10,
-#                  n_repeats=1):
-#    data=dataset.read_csv(in_path)
-#    protocol=base.get_protocol("unaggr")(n_splits,n_repeats)
-#    splits=base.DataSplits( data=data,
-#                            splits=protocol.get_split(data))
-#    clf_factory=ens.ClassEnsFactory(selected_classes=None)
-#    clfs=list(splits.get_clfs(clf_factory))
-#    lines=[]
-#    for subset_i in iter_subsets(data.n_cats()+1):
-#        s_clfs=[SelectedEns(clf_j,subset_i) for clf_j in clfs]
-#        results=splits.pred(s_clfs)
-#        mean_i,balance_i=np.mean(results.get_acc()),np.mean(results.get_balanced())
-#        lines.append([str(subset_i),mean_i,balance_i])
-#    df=pd.DataFrame.from_records(lines,columns=['subset','acc','balance'])
-#    return df   
-
-#def iter_subsets(n_clfs):
-#    cats= range(n_clfs)
-#    for i in range(1,n_clfs):
-#        cats_i=itertools.combinations(cats, i)
-#        for cats_j in cats_i:
-#            yield cats_j
-#    yield list(cats)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, default="../uci/vehicle")
@@ -123,6 +109,6 @@ if __name__ == '__main__':
         single_exp(in_path=args.input,
                out_path=args.output,
                ens_type=args.ens_type)
-        eval_exp(exp_path=args.output)
-    else:
-        history_exp(in_path=args.input)
+#        eval_exp(exp_path=args.output)
+#    else:
+#        history_exp(in_path=args.input)
