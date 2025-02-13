78c9a299a2f03fbb6953095b5d11787308ac02e4
tjacek <jacekt89@gmail.com>
2025-02-13 01:27:06 +0100

    full hist dict

--- eval.py
+++ eval.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 import utils
 
@@ -7,9 +8,19 @@ def history_acc(exp_path):
         for path_i in utils.top_files(in_path):
             history_i=f"{path_i}/history"
             if(os.path.exists(history_i)):
-                hist_dict=[utils.read_json(path_j) 
+                hist=[utils.read_json(path_j) 
                             for path_j in utils.top_files(history_i)]
-                print(hist_dict)
-    helper(exp_path)
+                keys=[key_j for key_j in hist[0].keys()
+                        if(not "loss" in key_j)]
+                hist_dict={ key_j:[hist_k[key_j] for hist_k in hist]
+                             for key_j in keys}
+                hist_dict={ key_j: (round(np.mean(value_j),4),
+                	                round(np.std(value_j),4))
+                             for key_j,value_j in hist_dict.items()}
+                return hist_dict
+    output_dict=helper(exp_path)
+    for name_i,dict_i in output_dict.items():
+    	print(name_i)
+    	print(dict_i)
 
 history_acc("new_exp")     
