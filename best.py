1ec06b3e0c586d4f8b57f8103942841d98bd8de4
tjacek <jacekt89@gmail.com>
2025-03-09 17:28:13 +0100

    Flexible Factory

--- best.py
+++ best.py
@@ -2,7 +2,41 @@ import numpy as np
 import pandas as pd 
 from collections import namedtuple
 from collections import defaultdict
-import base
+import base,dataset,ens,deep
+
+class FlexibleFactory(object):
+    def __init__(self,weight_gen,hyper_params=None):
+        if(hyper_params is None):
+            hyper_params={'layers':2, 'units_0':2,
+                          'units_1':1,'batch':False}
+        self.hyper_params=hyper_params
+        self.params=None
+        self.weight_dict=None
+        self.weight_gen=weight_gen
+
+    def init(self,data):
+        self.params={'dims': (data.dim(),),
+                     'n_cats':data.n_cats(),
+                     'n_epochs':500}
+        self.weight_dict=self.dataset.get_class_weights(data.y)
+
+    def __call__(self):
+        clfs=[]
+        for i in range(self.params["n_cats"]):
+            params_i= self.params.copy()
+            params_i['class_weights']=self.weight_gen(i,params_i)
+            clf_i=Deep(params=self.params,
+                       hyper_params=self.hyper_params)
+            clfs.appand(clf_i)
+        return NaiveEnsemble(clfs)
+
+class NaiveEnsemble(object):
+    def __init__(self,clfs):	
+        self.clfs=[]
+
+    def fit(self,X,y):
+        for model_i in self.clfs:
+            model_i.fit(X,y)
 
 def exp(in_path):
     data_split=base.get_splits(data_path=in_path,
