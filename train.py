39a32c2a686abbaa6cb240621803913ce15365ef
tjacek <jacekt89@gmail.com>
2025-03-21 22:48:05 +0100

    adaptative train

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ import utils
 utils.silence_warnings()
 import numpy as np
 from tqdm import tqdm
-#import gc#,ctypes
 import os.path
 import argparse,json
 import base,dataset,ens,utils
@@ -15,23 +14,24 @@ def base_train(data_path:str,
     path_dict=get_paths(out_path=out_path,
                         ens_type=ens_type,
                         dirs=['models','history','info.js'])
-    utils.make_dir(path_dict['ens'])
-    if(model_exist(path_dict['models'],start)):
-        raise Exception("Model exist")
+    utils.make_dir(path_dict['ens'])    
+    model_paths=get_model_paths(path_dict['models'],start,step)
+    print(model_paths)
+    if(len(model_paths)==0):
+        raise Exception("Models exist")
     utils.make_dir(path_dict['models'])
     utils.make_dir(path_dict['history'])
     data=dataset.read_csv(data_path)
     clf_factory=ens.get_ens(ens_type)
     clf_factory.init(data)
-    for j in tqdm(range(step)):
-        index=start+j
+    for index,model_path in tqdm(model_paths):
         raw_split=np.load(f"{path_dict['splits']}/{index}.npz")
         split_j=base.UnaggrSplit.Split(train_index=raw_split["arr_0"],
                                        test_index=raw_split["arr_1"])
         clf_j=clf_factory()
         clf_j,history_j=split_j.fit_clf(data,clf_j)  
         hist_dict_j=utils.history_to_dict(history_j)
-        clf_j.save(f"{path_dict['models']}/{index}.keras")
+        clf_j.save(model_path)
         with open(f"{path_dict['history']}/{index}", 'w') as f:
             json.dump(hist_dict_j, f)
     with open(path_dict['info.js'], 'w') as f:
@@ -45,22 +45,25 @@ def get_paths(out_path,ens_type,dirs):
     path_dir['splits']=f"{out_path}/splits"
     return path_dir
 
-def model_exist(model_path,start):
+def get_model_paths(model_path,start,step):
+    indexs=[start+j for j in range(step)]
+    paths=[ (index,f"{model_path}/{index}.keras") 
+               for index in indexs]
     if(not os.path.isdir(model_path)):
-         return False
-    first_path=f"{model_path}/{start}.keras"
-    if(not (os.path.isfile(first_path) or
-             os.path.isdir(first_path))):
-        return False
-    return True
+        return paths
+    paths=[ (i,model_i)
+              for i,model_i in paths
+                  if(not (os.path.isfile(model_i) or
+                         os.path.isdir(model_i)))]
+    return paths
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, default="../uci/cleveland")
-    parser.add_argument("--out_path", type=str, default="new_exp/cleveland")
-    parser.add_argument("--start", type=int, default=10)
-    parser.add_argument("--step", type=int, default=10)
-    parser.add_argument("--ens_type", type=str, default="separ_purity_ens")
+    parser.add_argument("--data", type=str, default="../uci/wall-following")
+    parser.add_argument("--out_path", type=str, default="new_exp/wine")
+    parser.add_argument("--start", type=int, default=28)
+    parser.add_argument("--step", type=int, default=100)
+    parser.add_argument("--ens_type", type=str, default="separ_class_ens")
     args = parser.parse_args()
     print(args)
     base_train(data_path=args.data,
