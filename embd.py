2be43688b320b7973e24c7508e8699675b027313
tjacek <jacekt89@gmail.com>
2025-04-06 12:54:21 +0200

    MultiReader class

--- embd.py
+++ embd.py
@@ -32,6 +32,10 @@ class NECSCF(object):
         return result
 
     def get_embd(self,X):
+        raise NotImplementedError()
+
+class MultiNECSCF(NECSCF):
+    def get_embd(self,X):
         if(self.extractor is None):
             self.extractor=Model(inputs=self.model.inputs, 
                                  outputs=list(self.get_outputs()))
@@ -48,6 +52,15 @@ class NECSCF(object):
             if(pattern.match(name_i)):
                 yield layer_i.output
 
+class MultiReader(object):
+    def __init__(self,ens_type):
+        self.ens_type=ens_type
+
+    def __call__(self,path):
+        model=tf.keras.models.load_model(path,
+                                        custom_objects={"loss":deep.WeightedLoss()})
+        return MultiNECSCF(model)
+
 def embd_exp(data_path,model_path):
     data=dataset.read_csv(data_path)
     acc=[]
@@ -62,15 +75,18 @@ def read_models(in_path,
                 step=10):
     split_path=f"{in_path}/splits"
     model_path=f"{in_path}/{ens_type}/models"
+    reader=MultiReader(ens_type)
     for index in range(step):
         i=start+index
         model_path_i=f"{model_path}/{i}.keras"
         print(model_path_i)
-        model_i=tf.keras.models.load_model(model_path_i,
-                                           custom_objects={"loss":deep.WeightedLoss()})
+#        model_i=tf.keras.models.load_model(model_path_i,
+#                                           custom_objects={"loss":deep.WeightedLoss()})
+        ens_i=reader(model_path_i)
         raw_split=np.load(f"{split_path}/{i}.npz")
         split_i=base.UnaggrSplit.Split(train_index=raw_split["arr_0"],
                                        test_index=raw_split["arr_1"])
-        yield i,NECSCF(model_i),split_i
+        yield i,ens_i,split_i
 
-embd_exp("../uci/cmc","new_exp/cmc")
+data='wine-quality-red'
+embd_exp(f"../uci/{data}",f"new_exp/{data}")
